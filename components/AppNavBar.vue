505bfb9260fb0b08a67ce7cd8627d202d6f788f0
DvaeFroot <ddjimenez2@up.edu.ph>
2025-02-12 21:01:41 +0800

    add: layout for navbar

--- components/AppNavBar.vue
+++ components/AppNavBar.vue
@@ -1,11 +1,24 @@
 <template>
-  <div class="flex flex-row">
-    <div class="flex flex-row">
-      <div>Logo</div>
-      <div>Navigation</div>
+  <div class="flex justify-between flex-row px-12 py-4 bg-gray-400">
+    <div class="flex flex-row justify-between bg-green-700 w-[600px]">
+      <div>Logo
+      </div>
+      <div class="flex flex-row justify-evenly ml-16 bg-red-100">
+        <div>
+          WHITELIST
+        </div>
+        <div>
+          UTILITY
+        </div>
+        <div>
+          STAKING
+        </div>
+      </div>
     </div>
-    <div>
-      <p>icons</p>
+    <div class="flex flex-row bg-pink-700">
+      <p>discord</p>
+      <p>telegram</p>
+      <p>twitter</p>
     </div>
   </div>
 </template>
